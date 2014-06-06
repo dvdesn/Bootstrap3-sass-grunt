9cc57bb122d3dc41f46d55c0a1397f79c37ecefb
davide <davide@Davide-Senatras-MacBook-Pro.local>
2014-06-06 14:34:09 +0200

    Installed Compass and folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 
 	grunt.initConfig({
 			uglify:{
@@ -10,6 +11,13 @@ module.exports = function(grunt) {
 					} //files
 				} //my_target
 			}, //uglify
+			compass: {
+				dev: {
+					options: {
+						config: 'config.rb'
+					}//options
+				}//dev
+			},//compass
 			watch: {
 				options: { livereload: true },
 				scripts: {
